f137ccf6e109a4d3698d1420107d7987486b1c31
Drew Harris <harrisd@smu.edu>
2023-11-09 00:43:06 -0600

    Revert orem

--- src/onStart.ts
+++ src/onStart.ts
@@ -23,31 +23,31 @@ export const onStart = async () => {
     }
   }
 
-  // let orem: string = "";
-  // const url = "http://bible.oremus.org/?version=NRSVAE&passage=Mark%201.1-2";
-  // const http = await fetch(url, {
-  //   method: "GET",
-  // })
-  //   .then(function (response) {
-  //     // The response is a Response instance.
-  //     // You parse the data into a useable format using `.json()`
-  //     return response.text();
-  //   })
-  //   .then(function (data) {
-  //     // `data` is the parsed version of the JSON returned from the above endpoint.
-  //     orem = data; // { "userId": 1, "id": 1, "title": "...", "body": "..." }
-  //   });
-  // if (!orem || orem == "") return;
-  // //remove all text before <h2 class="passageref">
-  // orem = orem.slice(orem.indexOf('<h2 class="passageref">'));
-  //
-  // //remove all text after </div><!-- class="bibletext" -->
-  // orem = orem.slice(0, orem.indexOf('</div><!-- class="bibletext" -->'));
-  //
-  // //remove all text between < and >
-  // //orem = orem.replace(/<[^>]*>?/gm, "");
-  //
-  // console.log(orem);
+  let orem: string = "";
+  const url = "http://bible.oremus.org/?version=NRSVAE&passage=Mark%201.1-2";
+  const http = await fetch(url, {
+    method: "GET",
+  })
+    .then(function (response) {
+      // The response is a Response instance.
+      // You parse the data into a useable format using `.json()`
+      return response.text();
+    })
+    .then(function (data) {
+      // `data` is the parsed version of the JSON returned from the above endpoint.
+      orem = data; // { "userId": 1, "id": 1, "title": "...", "body": "..." }
+    });
+  if (!orem || orem == "") return;
+  //remove all text before <h2 class="passageref">
+  orem = orem.slice(orem.indexOf('<h2 class="passageref">'));
+
+  //remove all text after </div><!-- class="bibletext" -->
+  orem = orem.slice(0, orem.indexOf('</div><!-- class="bibletext" -->'));
+
+  //remove all text between < and >
+  //orem = orem.replace(/<[^>]*>?/gm, "");
+
+  console.log(orem);
 
   /*
   const general = (await client.channels.fetch(

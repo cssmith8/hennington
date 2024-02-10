2bac10b8dea70dfe2a59f5701900380e48840cf8
BuildTools <unconfigured@null.spigotmc.org>
2024-02-09 22:55:31 -0600

    real comment

--- src/handleMessage.tsx
+++ src/handleMessage.tsx
@@ -56,6 +56,7 @@ export default async function handleMessage(message: Message<boolean>) {
     await new Promise((r) => setTimeout(r, 5000));
     //send a message to the channel
     message.channel.send("duo mobile be like");
+    //real
   }
 
   if (
